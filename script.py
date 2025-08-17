7cfe84babb5674e1ce6bc54013b1220244044004
Marcos Scurtu <132715054+Shurkon@users.noreply.github.com>
2025-08-17 19:14:29 +0300

    Actualizar script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@
 import yt_dlp
 
 
-# Función principañ
+# Función principal
 
 def main():
 
@@ -13,13 +13,12 @@ def main():
     print("\n[+] Por favor, selecciona una de las siguientes opciones: \n")
     print("\n[1] Descargar video a baja resolución")
     print("[2] Descargar video a alta resolución (Requiere tener instalado ffmepg)")
-    print("[3] Descargar audio mp3")
-    print("[4] Descargar audio mp4")
+    print("[3] Descargar audio en mp3")
 
 
     while True:   
         
-        opcion = int(input("\n\t Tu opción(1-4) --> "))
+        opcion = int(input("\n\t Tu opción(1-3) --> "))
         
         if opcion == 1:
 
@@ -40,13 +39,6 @@ def main():
             print("\n\n[i] Has elegido la opción nº3")
             enlace = input("[+] Introduce la URL del video: ")
             opcion3(enlace)
-            break
-
-        elif opcion == 4:
-
-            print("\n\n[i] Has elegido la opción nº4")
-            enlace = input("[+] Introduce la URL del video: ")
-            opcion4(enlace)
             break
 
         else:
@@ -89,23 +81,6 @@ def opcion3(url):
                 "key": "FFmpegExtractAudio",      # Extraer audio con FFmpeg
                 "preferredcodec": "mp3",          # Convertir a mp3
                 "preferredquality": "192",        # Calidad de audio
-            }
-        ],
-    }
-
-    with yt_dlp.YoutubeDL(opciones) as ydl:
-        ydl.download([url])
-
-
-
-def opcion4(url):
-    opciones = {
-        "format": "bestaudio/best",              # Solo audio en la mejor calidad
-        "outtmpl": "audios/%(title)s.%(ext)s",   # Carpeta y nombre automático
-        "postprocessors": [
-            {
-                "key": "FFmpegVideoConvertor",   # Convertir usando ffmpeg
-                "preferedformat": "mp4",         # Guardar como .mp4
             }
         ],
     }
